7e7ad52f0135336890dc3788bc879ba6d93e20cc
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-23 13:50:07 +0500

    Реализация вывода наибольшего и наименьшего бара

--- bars.py
+++ bars.py
@@ -5,18 +5,24 @@ def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
         parsed_json_data = json.load(f)
         print("json loaded")
-        for key, value in parsed_json_data.items():
-            for bar_params in value:
-                print(bar_params)
-    pass
+        # for key, value in parsed_json_data.items():
+        #     for bar_params in value:
+        #         print(bar_params)
+    return parsed_json_data
+
+bars_clean_list = []
 
 
 def get_biggest_bar(data):
-    pass
+    for bar_params in data['features']:
+        bars_clean_list.append(bar_params)
+    return max(bars_clean_list, key=lambda x:x['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
+    for bar_params in data['features']:
+        bars_clean_list.append(bar_params)
+    return min(bars_clean_list, key=lambda x:x['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(data, longitude, latitude):
@@ -27,4 +33,8 @@ if __name__ == '__main__':
     pass
 
 filepath = './bars.json'
-load_data(filepath)
+temp_data = load_data(filepath)
+
+print(get_biggest_bar(temp_data))
+print(get_smallest_bar(temp_data))
+
