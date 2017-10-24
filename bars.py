fa6138a5c1f8a5ca880241b588951a02e60863d3
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-24 16:33:23 +0500

    подправил наименование функций

--- bars.py
+++ bars.py
@@ -9,21 +9,21 @@ def load_data(filepath):
     return parsed_json_data
 
 
-def get_biggest_bar(parsed_json_data):
+def get_biggest_bar_name(parsed_json_data):
     return max(parsed_json_data['features'],
                key=lambda x: x['properties']
                ['Attributes']
                ['SeatsCount'])['properties']['Attributes']['Name']
 
 
-def get_smallest_bar(parsed_json_data):
+def get_smallest_bar_name(parsed_json_data):
     return min(parsed_json_data['features'],
                key=lambda x: x['properties']
                ['Attributes']
                ['SeatsCount'])['properties']['Attributes']['Name']
 
 
-def distance_counter(lat1, lon1, lat2, lon2):
+def distance_calculation(lat1, lon1, lat2, lon2):
     earth_radius = 6373.0
 
     dlon = lon2 - lon1
@@ -37,27 +37,27 @@ def distance_counter(lat1, lon1, lat2, lon2):
     return distance
 
 
-def get_closest_bar(parsed_json_data, longitude, latitude):
-    closest_bar_params = min(parsed_json_data['features'],
-                             key=lambda x: distance_counter
-                             (x['geometry']['coordinates'][0],
-                              x['geometry']['coordinates'][1],
-                              longitude,
-                              latitude))
-    return closest_bar_params['properties']['Attributes']['Name']
+def get_closest_bar_name(parsed_json_data, longitude, latitude):
+    closest_bar_info = min(parsed_json_data['features'],
+                           key=lambda x: distance_calculation
+                           (x['geometry']['coordinates'][0],
+                            x['geometry']['coordinates'][1],
+                            longitude,
+                            latitude))
+    return closest_bar_info['properties']['Attributes']['Name']
 
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
     parsed_json = load_data(filepath)
     print('Самый большой бар:')
-    print(get_biggest_bar(parsed_json))
+    print(get_biggest_bar_name(parsed_json))
     print('\n')
     print('Самый маленький бар:')
-    print(get_smallest_bar(parsed_json))
+    print(get_smallest_bar_name(parsed_json))
     print('\n')
     print('Чтобы узнать ближайший бар - укажите координаты:')
     lat2 = float(input('Введите ширину:\n'))
     lon2 = float(input('Введите долготу:\n'))
     print('Самый близкий бар:')
-    print(get_closest_bar(parsed_json, lat2, lon2))
+    print(get_closest_bar_name(parsed_json, lat2, lon2))
